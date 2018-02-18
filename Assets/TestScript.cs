6f6efcecb2a65a14886163b1ceff0e3b73e3839a
HeliSRaiN <lhelisrainl@gmail.com>
2018-02-18 23:47:23 +0300

    init

---
+++ Assets/TestScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	void Start () {
+        print("Первое включение testSctipt");
+	}
+	
+	void FixedUpdate () {
+        print(Time.fixedTime); // Просто выводим время работы движка
+	}
+}
